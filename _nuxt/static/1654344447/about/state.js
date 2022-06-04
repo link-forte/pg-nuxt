0b7234e0be052ff0655f3efc3ec886c090d72ece
link-forte <link-forte@users.noreply.github.com>
2022-06-04 12:07:28 +0000

    deploy: 32fda6d696a49fb05a888cefe92936db06b426d2

---
+++ _nuxt/static/1654344447/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpg-nuxt\u002F_nuxt\u002Fstatic\u002F1654344447",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fpg-nuxt\u002F",assetsPath:"\u002Fpg-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
